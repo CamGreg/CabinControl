deb94f8fd3d3dd198f7a6111e0338d23e5bb8d3e
IP-Cam <cam@industrialplankton.com>
2025-04-13 16:35:12 -0700

    blah

--- bom.md
+++ bom.md
@@ -4,17 +4,23 @@
 
 - ESP32-C3-DevKitC-02: https://www.digikey.ca/en/products/detail/espressif-systems/esp32-c3-devkitc-02u/15199039
 
-## Charge Controller Connector
+## Power
+
+- Power Supply: https://www.digikey.ca/en/products/detail/dfrobot/DFR1015/18069278
+
+## Charge Controller
+https://www.victronenergy.com/solar-charge-controllers/smartsolar-mppt-75-10-75-15-100-15-100-20
 
 - Housing x2: https://www.digikey.ca/en/products/detail/jst-sales-america-inc/PHR-4/608606
-- Crimped wire x8: https://www.digikey.ca/en/products/detail/jst-sales-america-inc/ASPHSPH24K152/6009458
-- Voltage shifter / serial selection: https://www.digikey.ca/en/products/detail/analog-devices-inc/ADUM1201ARZ-RL7/995629
+- Crimps x8: https://www.digikey.ca/en/products/detail/jst-sales-america-inc/SPH-002T-P0-5S/26216268
+- Wire: Any old ethernet will do
+- Voltage shifter / serial selection:
+  - https://www.digikey.ca/en/products/detail/analog-devices-inc/ADUM1201ARZ-RL7/995629
+  - https://www.digikey.ca/en/products/detail/skyworks-solutions-inc/SI8421BB-D-IS/2170663
 
 ## Control
 
-- Screen:
-  - https://www.digikey.ca/en/products/detail/adafruit-industries-llc/4440/10824271
-  - https://www.digikey.ca/en/products/detail/olimex-ltd/LCD-DISPLAY-16X2-BL/21662361
+- Screen: https://www.digikey.ca/en/products/detail/adafruit-industries-llc/4440/10824271
 - Mom Button x2 : https://www.digikey.ca/en/products/detail/adam-tech/SW-PB1-1BS-A-PK1-A/15284468
   - Screen
   - Wifi Toggle
@@ -22,9 +28,12 @@
 ## Sensors
 
 - Temperature:
-  - https://www.digikey.ca/en/products/detail/dfrobot/KIT0021/7087154
-  - https://www.amazon.com/HiLetgo-DS18B20-Temperature-Stainless-Waterproof/dp/B00M1PM55K
+  - DS18B20:
+    - https://www.digikey.ca/en/products/detail/dfrobot/KIT0021/7087154
+    - https://www.amazon.com/HiLetgo-DS18B20-Temperature-Stainless-Waterproof/dp/B00M1PM55K
+  - Victron Built in?: https://www.victronenergy.com/upload/documents/Datasheet-SmartSolar-charge-controller-MPPT-75-10,-75-15,-100-15,-100-20_48V-EN.pdf
 
 ## Recording
 
 - SD Card interface: https://www.digikey.ca/en/products/detail/digilent-inc/410-380/9445906
+  - Maybe just in ram/flash?
--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,48 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+const auto SDA = GPIO_NUM_0;
+const auto SCL = GPIO_NUM_0;
+
+const auto SCREEN_WIDTH = 128; // OLED display width, in pixels
+const auto SCREEN_HEIGHT = 32; // OLED display height, in pixels
+const auto OLED_RESET = -1;    // Reset pin # (or -1 if sharing Arduino reset pin)
+const auto refreshRate_ms = 1000 / 10;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 RTC_DATA_ATTR int64_t wifiTimer = 0; // decrement this while running, also decrement after sleep
 
 const char *ssid = "hello";
 const char *password = "12345678";
 WiFiServer server(80);
+void handleClient(WiFiClient client);
 
 const auto sleepPeriod_ms = 10 * 60 * 1000; // 10 minutes
 const auto WAKEUP_GPIO = GPIO_NUM_10;
-const auto WiFi_GPIO = GPIO_NUM_10;
+const auto WiFi_GPIO = GPIO_NUM_0;
+constexpr uint64_t BUTTON_PIN_BITMASK(uint64_t pin);
 
-const auto ONE_WIRE_BUS = GPIO_NUM_2;
+const auto OneWire_Power_GPIO = GPIO_NUM_0; // could be used for more than just OneWire, also might be negligible benefit
+const auto ONE_WIRE_BUS = GPIO_NUM_0;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void handleClient(WiFiClient client);
-constexpr uint64_t BUTTON_PIN_BITMASK(uint64_t pin);
+const auto ChargeCtrl_Select1_GPIO = GPIO_NUM_0;
+const auto ChargeCtrl_Select2_GPIO = GPIO_NUM_0;
+
+const auto OLED_Power_GPIO = GPIO_NUM_0;
 
 void setup()
 {
     // Pins
     pinMode(WAKEUP_GPIO, INPUT);
     pinMode(WiFi_GPIO, INPUT);
+    pinMode(OneWire_Power_GPIO, OUTPUT);
+    pinMode(ChargeCtrl_Select1_GPIO, OUTPUT);
+    pinMode(ChargeCtrl_Select2_GPIO, OUTPUT);
 
     if (digitalRead(WiFi_GPIO) == HIGH)
     {
@@ -58,6 +78,8 @@ void setup()
         Serial.println(myIP);
         server.begin();
     }
+
+    Wire.setPins(SDA, SCL);
 }
 
 void loop()
@@ -71,11 +93,22 @@ void loop()
         SensorsTimer = millis();
 
         // Temperature
+        digitalWrite(OneWire_Power_GPIO, HIGH);
         sensors.requestTemperatures();
         float temperature = sensors.getTempC(0, 2);
+        digitalWrite(OneWire_Power_GPIO, LOW);
 
         // Charge controller stats
+        digitalWrite(ChargeCtrl_Select1_GPIO, HIGH);
+        // Read charge controller1 information
+        // Serial.write();
+        digitalWrite(ChargeCtrl_Select1_GPIO, LOW);
+        digitalWrite(ChargeCtrl_Select2_GPIO, HIGH);
+        // Read charge controller2 information
+        // Serial.write();
+        digitalWrite(ChargeCtrl_Select2_GPIO, LOW);
     }
+
     // Screen Control
 
     // Wifi Control
@@ -88,6 +121,26 @@ void loop()
             handleClient(client);
         }
         // TODO: turn off wifi from user input
+    }
+
+    // OLED
+    static auto refreshTimer = millis();
+    if (digitalRead(WAKEUP_GPIO) == HIGH && millis() - refreshTimer > refreshRate_ms)
+    {
+        refreshTimer = millis();
+        digitalWrite(OLED_Power_GPIO, HIGH);
+        display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // configure display Voltage
+        display.clearDisplay();
+        // Write Charge status
+        display.setTextSize(2); // Draw 2X-scale text
+        display.setTextColor(WHITE);
+        display.setCursor(10, 0);
+        display.println(F("Text"));
+        display.display();
+    }
+    else if (digitalRead(WAKEUP_GPIO) == LOW) // turn off OLED
+    {
+        digitalWrite(OLED_Power_GPIO, LOW);
     }
 
     // Sleep
